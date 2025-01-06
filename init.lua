dfddb5312a8e5ed86091c8336934e7efe0a90d9f
Rustam Sharshenov <rustam@sharshenov.com>
2025-01-06 11:16:46 +0000

    Install vim-visual-multi & update plugins

--- init.lua
+++ init.lua
@@ -231,6 +231,8 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  'mg979/vim-visual-multi', -- Multiple cursors plugin
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
